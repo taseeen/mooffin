e92e0b00e00a8bb8b6319d66050415aff98f4aed
Rayan Mostofa <rayanmostofa50@gmail.com>
2021-10-31 00:28:27 -0700

    move events to events/ folder

--- main.js
+++ main.js
@@ -5,8 +5,13 @@ const { token } = require("./config.json");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
+
 const commandFiles = fs
   .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+const eventFiles = fs
+  .readdirSync("./events")
   .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
@@ -14,26 +19,13 @@ for (const file of commandFiles) {
   client.commands.set(command.data.name, command);
 }
 
-client.on("ready", () => {
-  console.log(`${client.user.tag} is online!`);
-});
-
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  const command = client.commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
   }
-});
+}
 
 client.login(token);
