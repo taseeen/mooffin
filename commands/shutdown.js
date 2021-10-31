8464cd5f98228072521a5d1d6b7921f8256af64b
Rayan Mostofa <rayanmostofa50@gmail.com>
2021-10-31 12:16:44 -0700

    add shutdown command

---
+++ commands/shutdown.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { ownerId } = require("../config.json");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("shutdown")
+    .setDescription("Shuts off the bot."),
+  async execute(interaction) {
+    if (interaction.member.id == parseFloat(ownerId)) {
+      await interaction.reply("Shutting down...");
+      console.log("Shutting down...");
+      await interaction.client.destroy();
+    } else {
+      interaction.reply({
+        content: "You can't do that!",
+        ephemeral: true,
+      });
+    }
+  },
+};
