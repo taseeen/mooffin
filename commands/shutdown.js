73e1c4ace7b1375b3c2f59f5aaa69d22f7714443
Rayan Mostofa <rayanmostofa50@gmail.com>
2021-10-31 17:09:26 -0700

    remove directories in commands/

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { ownerId } = require("../../config.json");
+const { ownerId } = require("../config.json");
 
 module.exports = {
   data: new SlashCommandBuilder()
